78022d7cda19adf04194c4ef06ceefb3f7fdad32
008kevin <77583631+008kevin@users.noreply.github.com>
2026-02-03 10:39:55 +0100

    több adat hozzáadása

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -3,5 +3,7 @@
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string TaxNumber { get; } = "123456789";
+        public int UnusedDaysOff { get; } = 50;
     }
 }
